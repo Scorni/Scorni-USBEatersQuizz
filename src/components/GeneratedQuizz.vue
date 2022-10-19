ac639ed548746be64ff5bdde4ae6d66de81cf7c1
Scorni <m.scorniciel@students.ephec.be>
2022-10-19 18:22:03 +0200

    started question's list's style refont

--- src/components/GeneratedQuizz.vue
+++ src/components/GeneratedQuizz.vue
@@ -1,65 +1,51 @@
 <template>
-  <div class="hello">
-    <el-row :gutter="20" justify="center">
-      <el-col  :span="12" class = "headers">
-          <h1>Your generated Quizz 💾 </h1>
-      </el-col>
-    </el-row>
-    <el-row :gutter="20" justify="center">
-      <el-col  :span="12" >
-          <div class="content" v-if="$route.params.category">
-            <span>Selected: {{ $route.params.category}} - {{$route.params.difficulty}}</span>
+  <div >
+    <div class="content" v-if="$route.params.category">
+      <span>Selected: {{ $route.params.category}} - {{$route.params.difficulty}}</span>
+    </div>
+    <div v-else-if="$route.params.tag">
+      <span>Selected: {{ $route.params.tag }} - {{ $route.params.difficulty}} </span>
+    </div>
+    
+    <div class="questionList">
+      <div class="questionHeader">
+        <span>Questions</span>
+      </div>
+      <div class="allQuestions">
+        <div v-for="value in questionGenerated" :key="value.question" v-bind:id="questionGenerated.indexOf(value)" class="question"  ref="question" >
+          <div class="questionsLink" :type="this.type[questionGenerated.indexOf(value)]" >
+            <router-link class="routerLink"
+            v-bind:type="type[questionGenerated.indexOf(value)]"
+            :to="{ 
+              name: 'question', 
+              params: 
+                { 
+                  question:JSON.stringify(value), 
+                  number: this.questionGenerated.indexOf(value),
+                  questionsList: JSON.stringify(this.questionGenerated), 
+                  successQuestion: successQuestion
+                }
+              } ">
+            {{this.questionGenerated.indexOf(value)}}</router-link>
           </div>
-          <div v-else-if="$route.params.tag">
-            <span>Selected: {{ $route.params.tag }} - {{ $route.params.difficulty}} </span>
-          </div>
-      </el-col>
-    </el-row>
-    <el-row :gutter="20" justify="center">    
-      <el-col :xs="12" :sm="6" :md="6" :lg="5" :xl="5">
-        <el-card class="box-card">
-            <template #header>
-            <div class="card-header">
-              <span>Question's List</span>
-            </div>
-          </template>
-          <div v-for="value in questionGenerated" :key="value.question"  v-bind:id="questionGenerated.indexOf(value)" ref="question" >
-            <el-button  class="questionLink" :type="this.type[questionGenerated.indexOf(value)]" round>
-              <router-link class="routerLink"
-              v-bind:type="type[questionGenerated.indexOf(value)]"
-              :to="{ 
-                name: 'question', 
-                params: 
-                  { 
-                    question:JSON.stringify(value), 
-                    number: this.questionGenerated.indexOf(value),
-                    questionsList: JSON.stringify(this.questionGenerated), 
-                    successQuestion: successQuestion
-                  }
-                } ">
-              Question n°{{this.questionGenerated.indexOf(value)}}</router-link>
-            </el-button>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
-    <el-row :gutter="20" justify="center">    
-      <el-col  :span="12" >
-        <div v-show="showResultLink">
-        <el-button class="questionLink" type="warning" round>
-          <router-link class="routerLink" :to="{
-            name : 'result',
-            params: {
-              answers : successQuestion
-            }
-          }">
-          To the result page
-          </router-link>
-        </el-button>
-        </div>
-      </el-col>
-    </el-row>
-        
+    </div>
+      </div>
+      
+    </div>
+      
+    
+    <div v-show="showResultLink">
+    <button class="questionLink" type="warning" round>
+      <router-link class="routerLink" :to="{
+        name : 'result',
+        params: {
+          answers : successQuestion
+        }
+      }">
+      To the result page
+      </router-link>
+    </button>
+    </div>
     </div>
 </template>
 
@@ -115,24 +101,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #926dde;
-}
-.questionLink{
-  margin: 5px;
-}
-.box-card{
-  background-color: #ebb563;
-}
+@import '../assets/style/Components/GeneratedQuizz/GeneratedQuizz';
+
 </style>
--- src/components/Quizz.vue
+++ src/components/Quizz.vue
@@ -1,59 +1,57 @@
 <template>
-    <div class="headers">
-      <div class="groupedHeaders">
-        <h1>Welcome</h1>
-        <h1>On this</h1>
-        <h1>Quizz.</h1>
-      </div>
-      
+  <div class="headers">
+    <div class="groupedHeaders">
+      <h1>Welcome</h1>
+      <h1>On this</h1>
+      <h1>Quizz.</h1>
     </div>
-    <div class="ghostBordersRight"></div>
-    <div class="ghostBordersBottom"></div>
-    <div class="headersDynamics">
-      <div class="groupedHeadersDynamics" ref="groupedHeadersDynamics">
-        <h1> {{headers[0][0]}} </h1>
-        <h1> {{headers[0][1]}} </h1>
-        <h1> {{headers[0][2]}} </h1>
-      </div>
+  </div>
+  <div class="ghostBordersRight"></div>
+  <div class="ghostBordersBottom"></div>
+  <div class="headersDynamics">
+    <div class="groupedHeadersDynamics" ref="groupedHeadersDynamics">
+      <h1> {{headers[0][0]}} </h1>
+      <h1> {{headers[0][1]}} </h1>
+      <h1> {{headers[0][2]}} </h1>
     </div>
-    <div class="ghostBordersSecondRight"></div>
-    <div class="ghostBordersSecondTop"></div>
-    <div class="options">
-        <form id="questionOptions">
-              <div class="content category">
-                <select v-model="categorySelected" id="categorySelected" ref="categorySelected" :disabled="categoryDisabled" placeholder="Category" clearable>
-                  <option value="" selected disabled data-default>Category</option>
-                  <option v-for="value in this.requestOptions.category" :key="value.category" :value="value" >
-                    {{value}}
-                  </option>
-                </select>
-              </div>
-              <div class="content tag">
-                <select v-model="tagSelected" id="tagSelected" ref="tagSelected"  :disabled="tagDisabled" clearable>
-                  <option value="" selected disabled data-default>Tag</option>
-                  <option v-for="value in this.requestOptions.tag" :key="value.tag" :value="value">
-                    {{value}}
-                  </option>
-                </select>
-              </div>
-              <div class="content difficulty">
-                <select v-model="difficultySelected" id="difficultySelected" >
-                  <option value="" selected disabled data-default>Difficulty</option>
-                  <option v-for="value in this.requestOptions.difficulty" :key="value.difficulty" :value="value">
-                    {{value}}
-                  </option>
-              </select>
-              </div>
-              <div>
-                <div class="generateQuizz" ref="generateQuizz" id="generateQuizz" hidden>
-                  <button  class="questionLink" round>
-                    <router-link class="routerLink" :to="{ name: 'generatedQuizz', params: { category : this.categorySelected, difficulty: this.difficultySelected, tag: this.tagSelected}}" >Generate Quizz
-                    </router-link>
-                  </button>
-                </div>
-              </div>
-              
-        </form>
+  </div>
+  <div class="ghostBordersSecondRight"></div>
+  <div class="ghostBordersSecondTop"></div>
+  <div class="options">
+    <form id="questionOptions">
+      <div class="content category">
+        <select v-model="categorySelected" id="categorySelected" ref="categorySelected" :disabled="categoryDisabled" placeholder="Category" clearable>
+          <option value="" selected disabled data-default>Category</option>
+          <option v-for="value in this.requestOptions.category" :key="value.category" :value="value" >
+            {{value}}
+          </option>
+        </select>
+      </div>
+      <div class="content tag">
+        <select v-model="tagSelected" id="tagSelected" ref="tagSelected"  :disabled="tagDisabled" clearable>
+          <option value="" selected disabled data-default>Tag</option>
+          <option v-for="value in this.requestOptions.tag" :key="value.tag" :value="value">
+            {{value}}
+          </option>
+        </select>
+      </div>
+      <div class="content difficulty">
+        <select v-model="difficultySelected" id="difficultySelected" >
+          <option value="" selected disabled data-default>Difficulty</option>
+          <option v-for="value in this.requestOptions.difficulty" :key="value.difficulty" :value="value">
+            {{value}}
+          </option>
+      </select>
+      </div>
+      <div>
+        <div class="generateQuizz" ref="generateQuizz" id="generateQuizz" hidden>
+          <button  class="questionLink" round>
+            <router-link class="routerLink" :to="{ name: 'generatedQuizz', params: { category : this.categorySelected, difficulty: this.difficultySelected, tag: this.tagSelected}}" >Generate Quizz
+            </router-link>
+          </button>
+        </div>
+      </div>
+    </form>
   </div>
   <div class="ghostBordersOptionsLeft"></div>
   <div class="ghostBordersOptionsBottom"></div>
@@ -89,13 +87,9 @@ export default {
     },
     swipeHeaders(){
       setInterval(() => {
-        let spliceHeaders = this.headers.splice(0,1);
-        this.headers.push(spliceHeaders[0])
-        this.$refs.groupedHeadersDynamics.style.animationDirection = "reverse";
-        this.$refs.groupedHeadersDynamics.style.animation = 'none';
-        this.$refs.groupedHeadersDynamics.offsetHeight; /* trigger reflow */
-        this.$refs.groupedHeadersDynamics.style.animation = null; 
-      }, 3000);
+          let spliceHeaders = this.headers.splice(0,1);
+          this.headers.push(spliceHeaders[0])
+      }, 6500);
     }
   },
   watch: {
@@ -123,4 +117,5 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import '../assets/style/Components/Quizz/Quizz.scss';
+
 </style>
